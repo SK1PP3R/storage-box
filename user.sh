14d2f9215e6c5ce7fef77673bfc310d04afa284c
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-03-19 17:45:08 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -8,44 +8,44 @@ fi
 
 if [ "$SMB_ENABLE" = true ]; then
         apt install samba -y
-sudo rm /etc/samba/smb.conf && sudo tee /etc/samba/smb.conf > /dev/null <<EOF
-[global]
-port = ${SMB_PORT}
-workgroup = WORKGROUP
-log file = /var/log/samba/log.%m
-max log size = 1000
-logging = file
-panic action = /usr/share/samba/panic-action %d
-server role = standalone server
-obey pam restrictions = yes
-unix password sync = yes
-passwd program = /usr/bin/passwd %u
-passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-pam password change = yes
-map to guest = bad user
-usershare allow guests = yes
-[homes]
-   comment = Home Directories
-   browseable = no
-   read only = yes
-   create mask = 0700
-   directory mask = 0700
-   valid users = %S
-[printers]
-   comment = All Printers
-   browseable = no
-   path = /var/spool/samba
-   printable = yes
-   guest ok = no
-   read only = yes
-   create mask = 0700
-[print$]
-   comment = Printer Drivers
-   path = /var/lib/samba/printers
-   browseable = yes
-   read only = yes
-   guest ok = no
-EOF
-fi
+        rm /etc/samba/smb.conf && tee /etc/samba/smb.conf > /dev/null <<EOF
+        [global]
+        port = ${SMB_PORT}
+        workgroup = WORKGROUP
+        log file = /var/log/samba/log.%m
+        max log size = 1000
+        logging = file
+        panic action = /usr/share/samba/panic-action %d
+        server role = standalone server
+        obey pam restrictions = yes
+        unix password sync = yes
+        passwd program = /usr/bin/passwd %u
+        passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+        pam password change = yes
+        map to guest = bad user
+        usershare allow guests = yes
+        [homes]
+           comment = Home Directories
+           browseable = no
+           read only = yes
+           create mask = 0700
+           directory mask = 0700
+           valid users = %S
+        [printers]
+           comment = All Printers
+           browseable = no
+           path = /var/spool/samba
+           printable = yes
+           guest ok = no
+           read only = yes
+           create mask = 0700
+        [print$]
+           comment = Printer Drivers
+           path = /var/lib/samba/printers
+           browseable = yes
+           read only = yes
+           guest ok = no
+        EOF
+        fi
 
 watch -n3 "ls -la /root"
