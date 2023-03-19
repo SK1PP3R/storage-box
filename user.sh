718a27b2ff6f1c501c8407568edb9b999af6370f
SK1PP3R <56693392+SK1PP3R@users.noreply.github.com>
2023-03-19 17:05:13 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,1 +1,7 @@
-
+#!/bin/sh
+if [ ! "$(id -u steam)" -eq "$UID" ]; then 
+        usermod -o -u "$UID" steam ; 
+fi
+if [ ! "$(id -g steam)" -eq "$GID" ]; then 
+        groupmod -o -g "$GID" steam ; 
+fi
